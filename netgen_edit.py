45a8c81bb30cb830f209b189b1f1a9a1d188fc00
Adam Rumpf <arumpf@hawk.iit.edu>
2020-01-27 14:17:30 -0500

    Implemented trial generation driver.

--- netgen_edit.py
+++ netgen_edit.py
@@ -12,9 +12,10 @@ The output files include additional information for defining interdependencies
 and defensible/destructible arc sets.
 
 The following scripts are included:
-    call_netgen -- Calls a standalone NETGEN .exe to generate an interdependent
-        network instance.
-    generate_trials -- Generates a set of trials
+    call_netgen() -- Calls a standalone NETGEN .exe to generate an
+        interdependent network instance.
+    generate_trials() -- Generates a set of trials with identical parameters
+        but different random seeds.
 """
 
 import math
@@ -34,7 +35,7 @@ def call_netgen(file, nodes, arcs, itype, inum, seed=None, sources=None,
     added to the file by the other scripts in this file.
 
     Requires the following positional arguments:
-        file -- Name of the output file.
+        file -- Name of the output file (excluding the file extension).
         nodes -- Number of nodes.
         arcs -- Number of arcs.
         itype -- Interdependency type (0 for parent nodes, 1 for parent arcs).
@@ -69,15 +70,71 @@ def call_netgen(file, nodes, arcs, itype, inum, seed=None, sources=None,
         supply = 10000 * math.ceil(nodes / 256)
 
     # Call the NETGEN program and return the exit code
-    return os.system("netgen.exe "+file+" "+str(seed)+" "+str(nodes)+" "+
+    return os.system("netgen.exe "+file+".min "+str(seed)+" "+str(nodes)+" "+
                      str(sources)+" "+str(sinks)+" "+str(arcs)+" "+
                      str(costs[0])+" "+str(costs[1])+" "+str(supply)+
                      " 0 0 100 100 "+str(capacities[0])+" "+str(capacities[1])+
                      " "+str(itype)+" "+str(inum))
 
 #==============================================================================
-def generate_trials():
-    pass
+def generate_trials(file, num, nodes, arcs, itype, inum, sources=None,
+                sinks=None, costs=(0, 100), supply=None,
+                capacities=(100, 500)):
+    """Generates a set of trials using the same parameters.
+
+    This function acts as a driver for call_netgen() to generate a set of
+    trials using the same set of parameters but different random seeds.
+
+    Requires the following positional arguments:
+        file -- Base name of the output file (excluding the file extension).
+            All generated trials will be given the specified name followed by a
+            unique number.
+        num -- Number of files to generate.
+        nodes -- Number of nodes.
+        arcs -- Number of arcs.
+        itype -- Interdependency type (0 for parent nodes, 1 for parent arcs).
+        inum -- Number of interdependencies.
+
+    Accepts the following optional keyword arguments:
+        sources -- Number of source nodes. Defaults to 20% of all nodes
+            (rounded up).
+        sinks -- Number of sink nodes. Defaults to 20% of all nodes (rounded
+            up).
+        costs -- Tuple of arc cost lower bound and upper bound. Defaults to
+            (0, 100).
+        supply -- Total supply value across all source nodes. Defaults to
+            10,000 per 256 nodes (rounded up).
+        capacities -- Tuple of arc capacity lower bound and upper bound.
+            Defaults to (100, 500).
+    """
+
+    # Set unspecified sources and sinks
+    if sources == None:
+        sources = math.ceil(0.2 * nodes)
+    if sinks == None:
+        sinks = math.ceil(0.2 * nodes)
+
+    # Set unspecified total supply
+    if supply == None:
+        supply = 10000 * math.ceil(nodes / 256)
+
+    # Main loop
+    i = 1
+    while i <= num:
+        # Generate file name by concatenating a number
+        fname = file + str(i).zfill(math.ceil(math.log10(num+1)))
+
+        # Use call_netgen() to generate a trial with an unspecified seed
+        code = call_netgen(fname, nodes, arcs, itype, inum, sources=sources,
+                sinks=sinks, costs=costs, supply=supply, capacities=capacities)
+
+        # If return code indicates unsuccessful execution, repeat loop
+        if code != 0:
+            continue
+
+        print("Trial "+str(i)+" / "+str(num)+" generated.")
+
+        i += 1
 
 #==============================================================================
 def convert_folder():
